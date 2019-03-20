73b158cf0a0ff2faa3c8725951c4a5cac2f3d455
middle <630390782@qq.com>
2019-03-20 15:33:17 +0800

    完善html-tempalte, 修改clean-webpack-plugin版本

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ https://docs.npmjs.com/files/package.json npm-package文档
 问题：
 1. 初始化 npm install webpack -D会报错.
    package.json不能有webpack的字眼，修改name：webpack -> webpack-project
+2. clean-webpack-plugin 版本问题
+   2.x.x 参数不支持path，改为1.x.x
 
 技术：
 1. git的.gitignore文件 忽略文件提交到github上去
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,23 @@
 const path = require('path')
+const fs = require('fs')
 // html-webpack-html 生成默认index.html
 // 文档： https://github.com/jaketrent/html-webpack-template
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 // clean-webpack-plugin 清除dist下的所有文件
 // 文档： https://github.com/johnagan/clean-webpack-plugin
 const CleanWebpackPlugin = require('clean-webpack-plugin')
+// 读取文件 遍历文件名称数据
+let html = fs.readdirSync(path.resolve(__dirname, './public/view')).map((filename) => 
+    // 返回 HtmlWebpackPlugin 实例    
+    new HtmlWebpackPlugin({
+        // 是否注入文件（Boolean）
+        inject: false,
+        // 输出的文件名称
+        filename: path.resolve(__dirname, `./dist/view/${filename}`),
+        // 使用的模板
+        template: path.resolve(__dirname, `./public/view/${filename}`)
+    })
+)
 
 module.exports = {
     // 开发环境
@@ -13,20 +26,18 @@ module.exports = {
     devtool: 'inline-source-map',
     // entry 输入文件
     entry: {
-        app: './public/js/app.js',
-        index: './public/js/index.js',
+        common: path.resolve(__dirname, './public/js/common.js')
     },
     // output 输出文件
     output: {
-        // 输出文件路径
-        path: path.resolve(__dirname, 'dist'),
-        // 输出文件名称
-        filename: '[name].bundle.js'
+        path: path.resolve(__dirname, './dist/js'),
+        filename: '[name].js'
     },
+    // plugins 使用的插件
     plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            title: '管理输出'
-        })
+        // 清楚文件下所有文件
+        new CleanWebpackPlugin(['dist']),
+        // 拼接html -> HtmlWebpackPlugin
+        ...html
     ]
 }
